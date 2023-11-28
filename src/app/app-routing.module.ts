951043c411a42252eaab0f3bbed74be18e3bfc0a
Abhishek Jadhav <81374155+abhijadhav9850@users.noreply.github.com>
2023-11-28 15:37:42 +0530

    add routing in food component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LogininphoneComponent } from './logininphone/logininphone.component';
 import { DetailsComponent } from './details/details.component';
 import { OtpVerificationComponent } from './otp-verification/otp-verification.component';
 import { LogindetailsComponent } from './logindetails/logindetails.component';
+import { FoodQuantityComponent } from './food-quantity/food-quantity.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
@@ -12,6 +13,8 @@ const routes: Routes = [
   {path:'login',component:LogininphoneComponent},
   {path:'otp',component:OtpVerificationComponent},
   {path:'login-details',component:LogindetailsComponent},
+  {path:'food', component:FoodQuantityComponent},
+
 
 ];
 
