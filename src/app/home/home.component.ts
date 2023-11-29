2b61885b26264c07515cdff431161c5b889508eb
rudraBelwalkar <144021775+rudraBelwalkar@users.noreply.github.com>
2023-11-29 18:16:24 +0530

    created details page ui and popup animation added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  hide : any = false
+
+  divStyle = {
+    // display :'none',
+    backgroundColor: 'lightblue',
+    color: 'black',
+    'margin-top': '45%',
+    'transition' : 'margin-top 1s',
+  };
+
+  changeStyle() {
+    this.hide =!this.hide
+    // Change the style dynamically
+
+    this.divStyle = {
+      // display: 'flex',
+      backgroundColor: 'lightgreen',
+      color: 'white',
+      'margin-top': '20%',
+      'transition': 'all 1s',
+    };
+  }
 }
