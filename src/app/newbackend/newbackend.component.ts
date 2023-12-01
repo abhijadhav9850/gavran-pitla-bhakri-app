aa809702f9875ff34e3a1e5873f00bc4f47d8eba
shubham310798 <shubhamsalunkhe19202@gmail.com>
2023-12-01 12:39:09 +0530

    basic

---
+++ src/app/newbackend/newbackend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newbackend',
+  templateUrl: './newbackend.component.html',
+  styleUrls: ['./newbackend.component.css']
+})
+export class NewbackendComponent {
+
+}
