c10bf47b3f008ec9daf94cc8cf7e3731ad5381be
Mayur Maruti Talekar <144109738+mayurtalekar@users.noreply.github.com>
2023-12-04 18:33:34 +0530

    login chnages

--- src/app/logindetails/logindetails.component.ts
+++ src/app/logindetails/logindetails.component.ts
@@ -12,9 +12,9 @@ import { LoginindetailsValueService } from 'src/loginindetails-value.service';
 })
 export class LogindetailsComponent {
   myForm: FormGroup;
-  
-  
-  constructor(public service : PopupHandingService , public fb: FormBuilder, public ls:LoginindetailsValueService,public http:HttpClient){
+  submited=false
+
+  constructor(public service: PopupHandingService, public fb: FormBuilder, public ls: LoginindetailsValueService, public http: HttpClient) {
     this.myForm = this.fb.group({
       UserName: ['', [Validators.required]],
       UserAddress: ['', [Validators.required]],
@@ -22,25 +22,26 @@ export class LogindetailsComponent {
     });
 
   }
+
   submitForm() {
     Object.values(this.myForm.controls).forEach((control) => {
       control.markAsTouched();
       console.log(this.myForm)
     });
-
     if (this.myForm.valid) {
       console.log('Form submitted:', this.myForm.value);
     }
   }
-  valueget(){
-    this.ls.logindeatilsvalue.push(this.myForm.value)   
-    console.log(this.ls.logindeatilsvalue);
 
-    this.http.post('http://localhost:4000/User/Add',this.myForm.value).subscribe(e=>{
+
+  valueget() {
+    this.ls.logindeatilsvalue.push(this.myForm.value)
+    console.log(this.ls.logindeatilsvalue);
+    this.http.post('http://localhost:4000/User/Add', this.myForm.value).subscribe(e => {
       console.log(e);
-      
+
     })
 
-     
+
   }
 }
