cb26a963367a5fc5ad82f42bac74cf0d10b7aff0
Abhishek Jadhav <81374155+abhijadhav9850@users.noreply.github.com>
2023-12-05 16:02:57 +0530

    otp changes

--- src/app/logininphone/logininphone.component.ts
+++ src/app/logininphone/logininphone.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PopupHandingService } from 'src/popup-handing.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { LoginindetailsValueService } from 'src/loginindetails-value.service';
 
 
 @Component({
@@ -14,22 +15,33 @@ export class LogininphoneComponent {
   submited = false
   emailsend:any=this.service.email
   phoneForm: FormGroup;
-  constructor(public service : PopupHandingService , public fb: FormBuilder, public http:HttpClient){
+  constructor(public service : PopupHandingService , public fb: FormBuilder, public http:HttpClient, public ls:LoginindetailsValueService){
     this.phoneForm = this.fb.group({
-      number: ['', [Validators.required , Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)] ],
+      email: ['', [Validators.required , Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)] ],
     });
-
-    this.http.post("http://localhost:4000/User/EmailID",this.phoneForm.value).subscribe(e => {
-      console.log(e);
-    })
   }
+
   submitForm() {
     Object.values(this.phoneForm.controls).forEach((control) => {
       control.markAsTouched();
     });
     this.submited = true
+
+    
     if (this.phoneForm.valid) {
-      const formData = { email: this.phoneForm.value.email };    }
+      const formData = { Email_ID: this.phoneForm.value.email };
+      // console.log(formData);
+      this.http.post("http://localhost:4000/User/EmailID",formData).subscribe(e => {
+       this.ls.otpvalue=e
+       console.log(this.ls.otpvalue);
+       
+      }) 
+    }else{
+      console.log("hello");
+      
+    }
+    
   }
+  
 
 }
