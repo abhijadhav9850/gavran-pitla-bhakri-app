47f60550feff8a6a651b381b7e8238b5cf0afebf
Abhishek Jadhav <81374155+abhijadhav9850@users.noreply.github.com>
2023-12-11 19:22:03 +0530

    auth connection

--- src/loginindetails-value.service.ts
+++ src/loginindetails-value.service.ts
@@ -1,16 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { PopupHandingService } from './popup-handing.service';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 
 export class LoginindetailsValueService { 
-
-  constructor(public http:HttpClient, public fb:FormBuilder) {}
-
+  
+  constructor(public http:HttpClient, public fb:FormBuilder, public router: Router) {}
+  
+  otpnumber:any;
   bhakriquantity:any = "";
   pithlaquantity:any = "";
   orderPrice : any= "";
+  address : any = false;
+  payment : any = false;
+  otp :any = false;
+  authLoggedIn = new BehaviorSubject<boolean>(false);
+
 
   foodquantity:any = [
    { bhakri : this.bhakriquantity,
@@ -32,6 +41,24 @@ export class LoginindetailsValueService {
     console.log(this.orderlist);
     
   }
+
+  
+
+   otpverifyapi(){
+    this.http.post("http://localhost:4000/OTP/GetOTP",this.otpnumber).subscribe((e:any)=>{
+    if(e.message === 'Otp not valid'){
+      console.log('OTP is not valid');
+    }else{
+      console.log('Otp successful');
+      this.address = true;
+      this.otp = false
+      console.log("Work");
+      
+      this.authLoggedIn.next(true)
+      this.router.navigate(['order-his'])
+    }
+    })
+   }
 
   // getOrderPrice(){
   //   console.log(this.orderPrice);
