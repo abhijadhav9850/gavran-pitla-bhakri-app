f7ffc4b744894e78bd7dc238d4a970d69043aec9
Abhishek Jadhav <81374155+abhijadhav9850@users.noreply.github.com>
2023-12-12 13:33:43 +0530

    basic

--- src/loginindetails-value.service.ts
+++ src/loginindetails-value.service.ts
@@ -1,77 +1,89 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { PopupHandingService } from './popup-handing.service';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
-export class LoginindetailsValueService { 
-  
-  constructor(public http:HttpClient, public fb:FormBuilder, public router: Router) {}
-  
-  otpnumber:any;
-  bhakriquantity:any = "";
-  pithlaquantity:any = "";
-  orderPrice : any= "";
-  address : any = false;
-  payment : any = false;
-  otp :any = false;
+export class LoginindetailsValueService {
+
+  constructor(public http: HttpClient, public fb: FormBuilder, public router: Router) { }
+
+  adddata: any;
+  otpnumber: any;
+  bhakriquantity: any = "";
+  pithlaquantity: any = "";
+  orderPrice: any = "";
+  address: any = false;
+  payment: any = false;
+  otp: any = false;
   authLoggedIn = new BehaviorSubject<boolean>(false);
 
   show_home_popup = false
 
-  foodquantity:any = [
-   { bhakri : this.bhakriquantity,
-     pithla : this.pithlaquantity,
-     test : '',
-     totalPrice : this.orderPrice,
-  }
-  ]
+  foodquantity: any = 
+    {
+      bhakri: this.bhakriquantity,
+      pithla: this.pithlaquantity,
+      test: '',
+      totalPrice: this.orderPrice,
+    }
+  
 
-  orderlist : any = [
+  orderlist: any = []
 
-  ]
+  logindeatilsvalue: any = [];
+  userinformation: any = [];
 
-  logindeatilsvalue:any=[];
-  userinformation : any = [];
- 
 
-  order_list(){
+  order_list() {
     this.orderlist.push(this.foodquantity)
     console.log(this.orderlist);
-    
+
   }
 
-  show_modify_popup(){
-    if(this.show_home_popup == false){
+  show_modify_popup() {
+    if (this.show_home_popup == false) {
       this.show_home_popup = true
-    }else{
+    } else {
       this.show_home_popup = false
     }
   }
-  
 
-   otpverifyapi(){
-    this.http.post("http://localhost:4000/OTP/GetOTP",this.otpnumber).subscribe((e:any)=>{
-    if(e.message === 'Otp not valid'){
-      console.log('OTP is not valid');
-    }else{
-      console.log('Otp successful');
-      // this.address = true;
-      // this.otp = false
-      console.log("Work");
-      
-      
-      this.authLoggedIn.next(true)
-      // this.router.navigate(['order-his'])
-    }
+
+  async otpverifyapi() {
+    console.log(this.foodquantity);
+    
+    await this.http.post("http://localhost:4000/OTP/GetOTP", this.otpnumber).subscribe(async (e: any) => {
+      if (e.message === 'Otp not valid') {
+        console.log('OTP is not valid');
+      } else {
+        console.log('Otp successful');
+        console.log("Work");
+        this.order_list()
+        //  this.http.post("http://localhost:4000/Mobile_No/Add",this.adddata).subscribe(e=>{
+        //        console.log(e); 
+        //     })  
+
+        await this.http.post("http://localhost:4000/Order_Details", this.foodquantity).subscribe(e => {
+                 console.log(e); 
+              })  
+
+
+          this.authLoggedIn.next(true)
+          // this.router.navigate(['order-his'])
+        }
     })
-   }
+  }
 
   // getOrderPrice(){
   //   console.log(this.orderPrice);
   // }
   // getUserInformation(){}
+
+
+  addorder_data() {
+
+  }
 }
